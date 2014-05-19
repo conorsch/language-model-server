a9fb757ef659b71b5a7e4507039ebe02e6c6b878
Conor Schaefer <conor.schaefer@gmail.com>
2014-05-18 23:04:35 -0400

    whitespace fix

--- languagemodelserver/ngram.py
+++ languagemodelserver/ngram.py
@@ -5,10 +5,10 @@ from textwrap import dedent
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/teste.db'
 db = SQLAlchemy(app)
-
 
 class Ngram(db.Model):
     id = db.Column(db.Integer, primary_key=True)
