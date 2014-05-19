c2a5df4da8ed1bb3cd789ee7283a6fe513158373
Conor Schaefer <conor.schaefer@gmail.com>
2014-05-19 08:00:20 -0400

    enables verbose output for server

--- languagemodelserver/ngram.py
+++ languagemodelserver/ngram.py
@@ -7,6 +7,10 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/teste.db'
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['DEBUG'] = True
+app.config['TESTING'] = True
+
 db = SQLAlchemy(app)
 
 
--- languagemodelserver/server.py
+++ languagemodelserver/server.py
@@ -2,15 +2,16 @@
 from __future__ import print_function
 import sys
 
-from flask import Flask
-app = Flask(__name__)
-
-from ngram import Ngram, db
+from ngram import Ngram, db, app
 
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/ngram/<order>")
+def getNgramByOrder(order):
+    return Ngram.query.first_or_404()
 
 if __name__ == "__main__":
     app.run()
