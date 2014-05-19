6f555521dff1316ecfe888e24a83dbfb645ff170
Conor Schaefer <conor.schaefer@gmail.com>
2014-05-19 07:51:10 -0400

    cleans up comments in loaddb

--- languagemodelserver/loaddb.py
+++ languagemodelserver/loaddb.py
@@ -6,12 +6,9 @@ import ngram
 from ngram import Ngram, db
 
 
-testlm = 'output-counts/cna_tokenized_lower.lm'
-
 def loadNgramsToDatabase(lmFilepath, order):
     """Reads in LM, exports n-grams of order n to SQLite database."""
 
-    print("Creating session handler...")
     print("Inserting ngrams into database...")
     ngrams = ngram.getNgrams(lmFilepath, order)
     counter = 0
@@ -19,6 +16,7 @@ def loadNgramsToDatabase(lmFilepath, order):
         db.session.add(n)
         counter += 1
         
+        # Commit often, to avoid heavy memory consumption.
         if counter % 10000 == 0:
             db.session.commit()
             sys.stdout.write("\rNumber of %s-grams committed to database: %s"% (str(order), str(counter)))
@@ -28,6 +26,6 @@ def loadNgramsToDatabase(lmFilepath, order):
     db.session.close()
 
 if __name__ == '__main__':
+    testlm = 'output-counts/cna_tokenized_lower.lm'
     n = 2
     loadNgramsToDatabase(testlm, n)
-
