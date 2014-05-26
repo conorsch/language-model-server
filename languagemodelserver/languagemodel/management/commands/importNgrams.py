04fec7b4757df80bceca97054c3d7cca18d6adc7
Conor Schaefer <conor.schaefer@gmail.com>
2014-05-26 13:01:39 -0400

    saner var names

--- languagemodelserver/languagemodel/management/commands/importNgrams.py
+++ languagemodelserver/languagemodel/management/commands/importNgrams.py
@@ -1,27 +1,30 @@
 from django.core.management.base import NoArgsCommand
-import languagemodelserver.languagemodel.models as models
 import sys
+import os
+import languagemodelserver.settings as settings
+import languagemodelserver.languagemodel.models as models
 
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
         # now do the things that you want with your models here
-        print("Inserting ngrams into database...")
-        lmFilepath = '/home/conor/gits/language-model-server/output-counts/cna_tokenized_lower.lm'
+        baseDir = settings.BASE_DIR
+        lmFilepath = os.path.join(baseDir, 'output-counts', 'cna_tokenized_lower.lm')
         order = 2
-        ngrams = models.getNgrams(lmFilepath, order)
         counter = 0
+        print("Inserting ngrams into database...")
+        ngramArgs = models.getNgrams(lmFilepath, order)
 
         print("Creating empty list for StagingArea...")
-        stagingArea = list()
+        commitBatch = list()
 
-        for n in ngrams:
+        for n in ngramArgs:
             ngram = models.Ngram(**n)
-            stagingArea.append(ngram)
+            commitBatch.append(ngram)
             counter += 1
             
             # Commit often, to avoid heavy memory consumption.
             if counter % 1000 == 0:
-                models.Ngram.objects.bulk_create(stagingArea)
+                models.Ngram.objects.bulk_create(commitBatch)
                 sys.stdout.write("\rNumber of %s-grams committed to database: %s"% (str(order), str(counter)))
                 sys.stdout.flush()
 
