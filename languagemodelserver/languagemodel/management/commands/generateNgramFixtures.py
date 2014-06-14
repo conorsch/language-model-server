6af61a49140b88a3a68652c7c4c0f601b9bd73c3
Conor Schaefer <conor.schaefer@gmail.com>
2014-06-14 19:32:43 -0400

    fixes relative paths

--- languagemodelserver/languagemodel/management/commands/generateNgramFixtures.py
+++ languagemodelserver/languagemodel/management/commands/generateNgramFixtures.py
@@ -1,14 +1,16 @@
 from django.core.management.base import NoArgsCommand
 from django.core import serializers
 import languagemodelserver.languagemodel.models as models
+import languagemodelserver.settings as settings
 import sys
+import os
 
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
         # now do the things that you want with your models here
         order = 2
-        fixturesFile = '/home/mulhod/Dropbox/Docs/corpora/grammatical_error_detection_shared_task_2013/language-model-server/fixtures/'+ "%sgrams.json" % order
-        print("Generating fixtures from ngrams in database...")
+        fixturesFile = os.path.join(settings.BASE_DIR, 'fixtures', "%sgrams.json" % order)
+        print("Generating fixtures from %s-grams in database..." % order)
         with open(fixturesFile, 'w') as out:
             serializers.serialize("json", models.Ngram.objects.iterator(), stream=out)
 
