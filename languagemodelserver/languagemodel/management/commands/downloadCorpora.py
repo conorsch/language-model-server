d442ebfecd7757740968f09aed6199bcb7504ef5
Conor Schaefer <conor.schaefer@gmail.com>
2014-06-14 22:18:54 -0400

    prettier corpora downloading

--- languagemodelserver/languagemodel/management/commands/downloadCorpora.py
+++ languagemodelserver/languagemodel/management/commands/downloadCorpora.py
@@ -1,8 +1,10 @@
+from __future__ import print_function
 from django.core.management.base import NoArgsCommand
 import nltk
 
 
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
-        nltk.download('all-corpora')
-        print("Finished downloading corpora.")
+        print("Downloading all NLTK corpora... ", end="")
+        nltk.download('all-corpora', quiet=True)
+        print("done.")
