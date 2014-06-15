78f4c9e49cbab781157036b0e46d14e440d20ef4
Conor Schaefer <conor.schaefer@gmail.com>
2014-06-14 22:41:03 -0400

    cleans up bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,31 +17,9 @@ MSG
     exit 1;
 fi
 
-# Ensure we have a corpus to work with
-testcorpus='corpus/cna_tokenized_lower.txt'
-if [[ ! -f $testcorpus ]] ; then
-    cat <<MSG
-Dataset '$testcorpus' was not found. Add it, then try again.
-MSG
-    exit 2;
-fi
+python manage.py syncdb --noinput
+python manage.py downloadCorpora
+python manage.py prepareCorpora
+python manage.py importNgrams
 
-outputdir='output-counts'
-mkdir -p $outputdir
-
-countsfile="$outputdir/$(basename $testcorpus | perl -npe 's/(\.)(\w+)$/$1counts/')"
-languagemodel="$outputdir/$(basename $testcorpus | perl -npe 's/(\.)(\w+)$/$1lm/')"
-
-echo "Dataset: '$testcorpus'"
-
-echo -n "Generating ngram counts ($countsfile)..."
-ngram-count -text $testcorpus -sort -write $countsfile -order 5 -unk
-echo " done."
-
-echo "Building language model ($languagemodel)..."
-make-big-lm -read $countsfile -lm $languagemodel -name $languagemodel -order 5 -unk
-echo " done."
-
-# Don't bother starting server
-#echo "Running ngram server..."
-#ngram -server-port 3433 -order 5 -debug 3 -memuse -lm $languagemodel -unk
+exit 0
