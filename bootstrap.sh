f14c890fa1736c966c9f9b7cb2cf84e77c987413
Conor Schaefer <conor.schaefer@gmail.com>
2014-06-19 22:52:47 -0400

    enables automatic corpora downloading

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@
 # Assumes partial corpus is present, and SRILM is installed.
 
 python manage.py syncdb --noinput && \
-#python manage.py downloadCorpora
+python manage.py downloadCorpora
 python manage.py prepareCorpora && \
 python manage.py importNgrams
 exit 0
